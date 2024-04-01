74c96f6e10c32de82ed4a54dfd5b2630ce566741
sunny <sunnya2023@gmail.com>
2024-04-01 15:26:44 +0900

    API_URL from page -> constants

--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,4 +1,5 @@
-import { API_URL } from "../app/(home)/page";
+// import { API_URL } from "../app/(home)/page";
+import { API_URL } from "../app/constants";
 import styles from "../style/movie-info.module.css";
 
 export async function getMovie(id: string) {
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -1,4 +1,4 @@
-import { API_URL } from "../app/(home)/page";
+import { API_URL } from "../app/constants";
 import styles from "../style/movie-videos.module.css";
 async function getVideos(id: string) {
   //   console.log("fetching videos", Date.now());
